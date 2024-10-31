c3c19d5a0217d02ed7d6e52c80aa624a3d57cee2
Churchilli <141407524+churchillitos@users.noreply.github.com>
2024-10-31 17:16:30 +0800

    Update api.js

--- handles/api.js
+++ handles/api.js
@@ -3,5 +3,5 @@ module.exports = {
   nethApi: "https://nethwieginedev.vercel.app",
   jonelApi: "https://appjonellccapis.zapto.org",
   canvasApi: "https://api-canvass.vercel.app",
-  markApi: "https://markdevs69v2-679r.onrender.com",
+  markApi: "https://markdevs-last-api-vtjp.onrender.com",
 };
