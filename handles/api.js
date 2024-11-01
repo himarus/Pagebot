11d99f47bffb8be887e2e71d7a00f82f9d48d5ba
Churchilli <141407524+churchillitos@users.noreply.github.com>
2024-11-01 17:54:44 +0800

    Update api.js

--- handles/api.js
+++ handles/api.js
@@ -1,5 +1,6 @@
 module.exports = {
   joshWebApi: "https://joshweb.click", 
+  kenlie: "https://apiv2.kenliejugarap.com",
   nethApi: "https://nethwieginedev.vercel.app",
   jonelApi: "https://appjonellccapis.zapto.org",
   canvasApi: "https://api-canvass.vercel.app",
