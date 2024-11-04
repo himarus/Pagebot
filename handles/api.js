e1e47051b12fa694354814552c0d689069cd861d
Churchilli <141407524+churchillitos@users.noreply.github.com>
2024-11-04 13:06:41 +0800

    Update api.js

--- handles/api.js
+++ handles/api.js
@@ -1,5 +1,6 @@
 module.exports = {
-  joshWebApi: "https://joshweb.click", 
+  joshWebApi: "https://joshweb.click",
+  jerome: "https://jerome-web.gleeze.com",
   kenlie: "https://apiv2.kenliejugarap.com",
   nethApi: "https://nethwieginedev.vercel.app",
   jonelApi: "https://ccprojectapis.ddns.net",
