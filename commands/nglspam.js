07b1cbc8a7832dcf8b242cfc5de66fc768930468
Kupalpriv <churchillabing100@gmail.com>
2024-11-10 22:47:03 +0800

    Update nglspam.js

--- commands/nglspam.js
+++ commands/nglspam.js
@@ -25,7 +25,7 @@ module.exports = {
 
     try {
       // Encode message to handle spaces and special characters
-      const apiUrl = `${api.markApi}/nglspam?nglusername=&message=${encodeURIComponent(username)}&message=${encodeURIComponent(message)}&total=${total}`;
+      const apiUrl = `${api.mark2}/nglspam?nglusername=&message=${encodeURIComponent(username)}&message=${encodeURIComponent(message)}&total=${total}`;
 
       // Perform the API request
       const response = await axios.get(apiUrl);
