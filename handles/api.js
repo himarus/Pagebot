4ba827b65f3bab092fd46db3d0dd646c15bd5112
Kupalpriv <churchillabing100@gmail.com>
2025-02-22 10:57:49 +0800

    Update api.js

--- handles/api.js
+++ handles/api.js
@@ -7,7 +7,7 @@ module.exports = {
   jerome: "https://jerome-web.gleeze.com",
   kenlie: "https://apiv2.kenliejugarap.com",
   nethApi: "https://nethwieginedev.vercel.app",
-  jonelApi: "https://jonellccapisbkup.gotdns.ch",
+  jonel: "https://ccprojectapis.ddns.net",
   canvasApi: "https://api-canvass.vercel.app",
   mark2: "https://apis-markdevs69v2.onrender.com",
   markApi: "https://markdevs-last-api-vtjp.onrender.com",
