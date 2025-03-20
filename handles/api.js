d84d1d375184426756bea82e954ade3db4b0c470
Kupalpriv <churchillabing100@gmail.com>
2025-03-20 18:44:14 +0800

    Update api.js

--- handles/api.js
+++ handles/api.js
@@ -1,14 +1,10 @@
 module.exports = {
-  joshWebApi: "https://api.zetsu.xyz",
   hazey: "https://hazeyyyy-rest-apis.onrender.com",
-  heru: "https://heru-apis.gleeze.com",
   kaizen: "https://kaiz-apis.gleeze.com",
-  kenlie2: "https://api.kenliejugarap.com",
-  jerome: "https://jerome-web.gleeze.com",
-  kenlie: "https://apiv2.kenliejugarap.com",
   nethApi: "https://nethwieginedev.vercel.app",
   jonel: "https://ccprojectapis.ddns.net",
   canvasApi: "https://api-canvass.vercel.app",
+  yakzy: "https://betadash-api-swordslush-production.up.railway.app",
   mark2: "https://apis-markdevs69v2.onrender.com",
   markApi: "https://markdevs-last-api-vtjp.onrender.com",
 };
