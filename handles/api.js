1dd0a135eb2fced7019d228e0aedb50cdb216cb2
Kupalpriv <churchillabing100@gmail.com>
2025-03-27 23:16:07 +0800

    Update api.js

--- handles/api.js
+++ handles/api.js
@@ -5,6 +5,5 @@ module.exports = {
   jonel: "https://ccprojectapis.ddns.net",
   canvasApi: "https://api-canvass.vercel.app",
   yakzy: "https://betadash-api-swordslush-production.up.railway.app",
-  mark2: "https://apis-markdevs69v2.onrender.com",
-  markApi: "https://markdevs-last-api-vtjp.onrender.com",
+  zaik: "https://renzsuperb.onrender.com",
 };
