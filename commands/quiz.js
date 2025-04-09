d55da7b87d36f8b8e14864803be3274d68368e9a
Kupalpriv <churchillabing100@gmail.com>
2025-04-10 00:04:02 +0800

    Update quiz.js

--- commands/quiz.js
+++ commands/quiz.js
@@ -5,7 +5,7 @@ module.exports = {
   name: 'quiz',
   description: 'Get a fun quiz question',
   usage: 'quiz',
-  author: 'chill',
+  author: 'yazky',
 
   async execute(senderId, args, pageAccessToken) {
     const apiUrl = 'https://betadash-api-swordslush-production.up.railway.app/quiz';
@@ -24,19 +24,10 @@ module.exports = {
 
       const message = {
         text: `📚 Category: ${quizData.category}\n🔄 Difficulty: ${quizData.difficulty}\n\n${quizData.question}`,
-        quick_replies: quickReplies.slice(0, 11) 
+        quick_replies: quickReplies.slice(0, 11) // Hanggang 11 quick replies
       };
 
       await sendMessage(senderId, message, pageAccessToken);
-
-      await sendMessage(senderId, {
-        attachment: {
-          type: 'audio',
-          payload: {
-            url: 'https://gamma.123tokyo.xyz/get.php/c/b2/eOKLFwPBBsI.mp3?n=%F0%9F%93%BA%20Family%20Feud%20Round%20Win%20Sound%20Effect%20%F0%9F%93%BA%20Game%20Show%20Sound%20Effects%20%E2%9C%85%20FREE%20TO%20USE%20%E2%9C%85&uT=R&uN=c3Ryb21mYXV6aWU%3D&h=tx-l9651m1qHbnjMKIePEA&s=1744220605&uT=R&uN=c3Ryb21mYXV6aWU%3D'
-          }
-        }
-      }, pageAccessToken);
 
     } catch (error) {
       console.error('Quiz error:', error);
