cb824ba56cfdabcdaced304aaf96db436def4884
Kupalpriv <churchillabing100@gmail.com>
2025-05-19 21:02:42 +0800

    Update api.js

--- handles/api.js
+++ handles/api.js
@@ -4,10 +4,13 @@ module.exports = {
   josh: "https://api.zetsu.xyz",
   haji: "https://haji-mix.up.railway.app",
   rapid: "https://rapido.zetsu.xyz",
-  kaizen: "https://kaiz-apis.gleeze.com",
+  kaizen: {
+    base: "https://kaiz-apis.gleeze.com",
+    key: "5ec89aab-a303-4d3f-8030-ba329a2ff201"
+  },
   nethApi: "https://nethwieginedev.vercel.app",
   jonel: "https://jonell01-ccprojectsapihshs.hf.space",
   canvasApi: "https://api-canvass.vercel.app",
   yakzy: "https://betadash-api-swordslush-production.up.railway.app",
-  zaik: "https://zaikyoov3-up.up.railway.app",
+  zaik: "https://zaikyoov3-up.up.railway.app"
 };
